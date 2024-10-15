094423781bdf0aa8173863277174cdcc9e744d2d
Akiraaa07 <igorakira2004@gmail.com>
2024-10-15 11:32:01 -0300

    Criando testes para post, put e delete

--- index.js
+++ index.js
@@ -42,3 +42,70 @@ async function TesteGetById(id){
         console.log(error);
     }
 }
+
+async function TestePost(carro) {
+    console.log('Iniciando o teste do POST!');
+    try {
+        let response = await fetch('https://apigenerator.dronahq.com/api/hVrn-n1A/carro', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(carro),
+        });
+
+        if (response.ok) {
+            let novoCarro = await response.json();
+            console.log('Carro criado:', novoCarro);
+        } else {
+            console.log('Erro na requisição:', response.status);
+        }
+        console.log('Terminou a requisição!');
+    } catch (error) {
+        console.log('Caiu no catch');
+        console.log(error);
+    }
+}
+
+async function TestePut(id, carro) {
+    console.log("Iniciando o teste do PUT!");
+    try {
+        let response = await fetch(`https://apigenerator.dronahq.com/api/hVrn-n1A/carro/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(carro),
+        });
+        if (response.ok) {
+            let carroAtualizado = await response.json();
+            console.log('Carro Atualizado: ', carroAtualizado);
+        } else {
+            console.log('Erro na requisição: ', response.status);
+        }
+    } catch (error) {
+        console.log('Caiu no catch!');
+        console.log(error)
+    }
+}
+
+async function TesteDelete(id) {
+    console.log("Iniciando o teste do DELETE!")
+    try {
+        let response = await fetch(`https://apigenerator.dronahq.com/api/hVrn-n1A/carro/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if(response.ok) {
+            console.log(`Carro com ID ${id} deletado com sucesso!`);
+        } else {
+            console.log('Erro na requisição: ', response.status);
+        }
+        
+    } catch (error) {
+        console.log('Caiu no catch!');
+        console.log(error);
+    }
+}
